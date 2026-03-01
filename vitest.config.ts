0bd9146ddc6755471e771631616a1b8f8a209391
Maria H <maria.hellsen@gmail.com>
2026-03-01 21:18:47 +0100

    Added axco-solution base to vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+   base: "/AXCO_Solutions-/",
   plugins: [react()],
   test: {
     environment: "jsdom",
