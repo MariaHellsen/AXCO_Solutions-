831c296481c30e130224ec012d2752f78a480e5e
Maria H <maria.hellsen@gmail.com>
2026-03-01 21:23:07 +0100

    Added base to vite.config instead

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-   base: "/AXCO_Solutions-/",
-  plugins: [react()],
+    plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
